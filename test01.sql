5794eb2c3cb091826553b09d06525d1f2eede5a1
leonardodeassis1999 <assisleonardo431@gmail.com>
2026-03-19 22:13:34 -0300

    Add files via upload

---
+++ test01.sql
@@ -0,0 +1,8 @@
+CREATE TABLE fornecedor (
+cnpj BIGINT(14) PRIMARY KEY NOT NULL,
+nomedaempresa VARCHAR (45),
+telefone BIGINT (09),
+endereco TEXT,
+email VARCHAR(45),
+especialidade VARCHAR (45)
+);
