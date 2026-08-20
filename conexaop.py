62c0f32aa8957872da8211b8628e3e674f482d58
Thiagozonta <zonta.thiago3@gmail.com>
2026-08-20 19:26:44 -0300

    trabalho_salvo

---
+++ conexaop.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+def conectar():
+    return mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="",
+        database="filmesbd"
+    )
